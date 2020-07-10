7c34369dd2303d3b0ddaa1a54271b8cc1c23582c
jh2song <spec0003@naver.com>
2020-07-10 17:07:48 +0900

    2020-07-10-17:07

---
+++ 2020-07-10/01/01/src/MenuEx.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class MenuEx extends JFrame {
+	public MenuEx() {
+		setTitle("Menu 만들기 예제");
+		createMenu();
+		setSize(250,200);
+		setVisible(true);
+	}
+	
+	private void createMenu() {
+		JMenuBar mb = new JMenuBar();
+		JMenu screenMenu = new JMenu("Screen");
+		
+		screenMenu.add(new JMenuItem("Load"));
+		screenMenu.add(new JMenuItem("Hide"));
+		screenMenu.add(new JMenuItem("ReShow"));
+		screenMenu.addSeparator();
+		screenMenu.add(new JMenuItem("Exit"));
+		
+		mb.add(screenMenu);
+		mb.add(new JMenu("Edit"));
+		mb.add(new JMenu("Source"));
+		mb.add(new JMenu("Project"));
+		mb.add(new JMenu("Run"));
+		
+		setJMenuBar(mb);
+	}
+	
+	public static void main(String[] args) {
+		new MenuEx();
+	}
+
+}
---
+++ 2020-07-10/01/01/src/TextAreaEx.java
@@ -0,0 +1,35 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.awt.*;
+
+public class TextAreaEx extends JFrame {
+	private JTextField tf = new JTextField(20);
+	private JTextArea ta = new JTextArea(7, 20);
+	
+	public TextAreaEx() {
+		setTitle("텍스트영역 만들기 예제");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Container c = getContentPane();
+		c.setLayout(new FlowLayout());
+		
+		c.add(new JLabel("읿력 후 <Enter> 키를 입력하세요"));
+		c.add(tf);
+		c.add(new JScrollPane(ta));
+		
+		tf.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JTextField t = (JTextField)e.getSource();
+				ta.append(t.getText() + "\n");
+				t.setText("");
+			}
+		});
+		
+		setSize(300,250);
+		setVisible(true);
+	}
+	
+	public static void main(String[] args) {
+		new TextAreaEx();
+	}
+
+}
